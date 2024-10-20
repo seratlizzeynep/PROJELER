06a33aee83b641e1bcce9af53ad609fa3d9b6ef2
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-10-20 21:16:51 +0300

    Add files via upload

---
+++ IntelijKisayollari.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class IntelijKisayollari {
+    public static void main(String[] args) {
+        // kod tamamlama
+        import java.util.ArrayList;
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("tamamlandi");
+        // kodu bu sekilde yazdiysak:
+        {
+            ArrayList<String> list2 = new ArrayList<>();
+            list2t.add("Merhaba");
+        }
+        // ctrl alt L yaparsak:
+
+
+        ArrayList<String> list3 = new ArrayList<>();
+        list.add("Merhaba");
+
+        // metod tanimina gitme;
+
+        printMessage(); // Buradaki metod adına `Ctrl + B` veya `Ctrl + Sol Tık` yaparsak metod tanımına gideriz.
+    }
+
+    public static void printMessage() {
+        System.out.println("Mesaj: 1");
+    }
+    // kod hatasını duzeltme;
+
+
+    int number = "123"; // Bu satırda int deger girmek gerekirken string girilmis.
+    // ctrl alt yaparsk;
+    int number = Integer.parseInt("123");
+
+    // kod basitleştirme;
+
+    int x = 0;
+                   if(x ==0)
+
+    {
+        System.out.println("Sıfır");
+    }
+    // alt enter yaparsak;
+        System.out.println("Sıfır");
+
+
+}
+
+
